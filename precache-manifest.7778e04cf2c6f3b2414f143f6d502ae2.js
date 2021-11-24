a8e8a19b5c139aab9dbbcc524835b65979ce0f89
JohnnyQr <qiaorenjohnny@wustl.edu>
2021-11-24 17:27:31 -0600

    Updates

---
+++ precache-manifest.7778e04cf2c6f3b2414f143f6d502ae2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9b26d5be33ffa1b7f77",
+    "url": "/09-todo-react-group-johnny-che/static/css/main.1af24ae9.chunk.css"
+  },
+  {
+    "revision": "f9b26d5be33ffa1b7f77",
+    "url": "/09-todo-react-group-johnny-che/static/js/main.f9b26d5b.chunk.js"
+  },
+  {
+    "revision": "0cdd6f184db929cffcf5",
+    "url": "/09-todo-react-group-johnny-che/static/js/1.0cdd6f18.chunk.js"
+  },
+  {
+    "revision": "3bf2c31bdeed6027a89f",
+    "url": "/09-todo-react-group-johnny-che/static/js/runtime~main.3bf2c31b.js"
+  },
+  {
+    "revision": "a2562b83d560b4690b2db65264a60bae",
+    "url": "/09-todo-react-group-johnny-che/index.html"
+  }
+];
